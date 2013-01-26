9c4c550965bcb983c997f8baf3fb6c700f4454f6
Keldon Alleyne <keldon.alleyne@avasopht.com>
2013-01-26 17:13:11 +0000

    Turned Bcon into an interface, with abstract BconAdapter extending the interface.

---
+++ src/main/java/bacn/BconAdapter.java
@@ -0,0 +1,78 @@
+package bacn;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
+
+public abstract class BconAdapter implements Bcon {
+  /**
+   * Returns a value that is registered as a member of `this` instance.
+   * @param clazz
+   * @param key
+   * @return
+   */
+  final protected <T extends Bcon> Value<T> member (Class<T> clazz, String key) {
+    Value<T> value = Value.create(clazz);
+    mValues.put(key, value);
+    return value;
+  }
+  
+  /**
+   * Returns a primitive value that is registered as a member of `this` instance.
+   * @param key
+   * @return
+   */
+  final protected <T extends Comparable<T>> Prim<T> member(String key) {
+    Prim<T> primitive = new Prim<T>();
+    mPrimitives.put(key, primitive);
+    return primitive;
+  }
+  
+  /**
+   * Extends `this` object with the type of T.
+   * @param clazz
+   * @param key
+   * @return
+   */
+  final protected <T extends Bcon> Value<T> extension (Class<T> clazz) {
+    Value<T> value = Value.create(clazz);
+    mExtensions.put(clazz.getCanonicalName(), value);
+    return value;
+  }
+  
+  /* (non-Javadoc)
+   * @see bacn.Bcon#getExtension(java.lang.String)
+   */
+  @Override
+  final public Value<?> getExtension(String key) {
+    return mExtensions.get(key);
+  }
+  
+  /* (non-Javadoc)
+   * @see bacn.Bcon#getExtensionKeys()
+   */
+  @Override
+  final public Collection<String> getExtensionKeys() {
+    return mExtensions.keySet();
+  }
+  
+  /* (non-Javadoc)
+   * @see bacn.Bcon#getValueKeys()
+   */
+  @Override
+  final public Collection<String> getValueKeys() {
+    return mValues.keySet();
+  }
+  
+  /* (non-Javadoc)
+   * @see bacn.Bcon#getPrimitiveKeys()
+   */
+  @Override
+  final public Collection<String> getPrimitiveKeys() {
+    return mPrimitives.keySet();
+  }
+  
+  private Map<String,Value<? extends Bcon>> mExtensions = new TreeMap<String,Value<? extends Bcon>>();
+  private Map<String,Value<? extends Bcon>> mValues = new TreeMap<String,Value<? extends Bcon>>();
+  private Map<String,Prim<?>> mPrimitives = new TreeMap<String, Prim<?>>();
+}
